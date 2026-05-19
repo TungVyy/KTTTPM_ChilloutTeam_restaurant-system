d184f14b4dddcb30ef1998a85ff9e2a5574c21df
Hoai An <nguyenhoaianss1502@gmail.com>
2026-05-19 20:29:42 +0700

    ui nhan vien bep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import Sidebar from "./components/admin/Sidebar.jsx";
-import EmployeePage from "./pages/admin/EmployeePage.jsx";
-import MenuPage from "./pages/admin/MenuPage.jsx";
+import React from 'react'
+import AppRoutes from './routes/AppRoutes'
+import './App.css'
 
 export default function App() {
-  const [page, setPage] = useState("employee");
-
-  return (
-    <div className="app">
-      <Sidebar page={page} setPage={setPage} />
-
-      {page === "employee" && <EmployeePage />}
-      {page === "menu" && <MenuPage />}
-    </div>
-  );
+  return <AppRoutes />
 }
